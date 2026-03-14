64c2f0e3542979bc6f8dc995699c866a31d0e1bd
HEY-Iam-SIBASANKAR <sankarsiba47@gmail.com>
2026-03-15 00:25:35 +0530

    fix cors policy

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin:ENV.CLIENT_URL,
-    credentials:true
+    origin: [ENV.CLIENT_URL, "http://localhost:5173"],
+    credentials:true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))

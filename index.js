6a073c0908107e83226128f8a3e240d8b5d69ae0
HEY-Iam-SIBASANKAR <sankarsiba47@gmail.com>
2026-03-15 00:46:08 +0530

    fix cors headers and options

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ import analyticRoute from './src/routes/analytic.route.js'
 import cors from 'cors'
 
 const app = express()
+app.set('trust proxy', 1)
 
 app.use(cors({
     origin: [ENV.CLIENT_URL, "http://localhost:5173"],
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization","Cookie"]
 }))
+
+app.options('*', cors())
 
 app.use(cookieParser())
 app.use(express.json())
